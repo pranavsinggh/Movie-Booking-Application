c4739fd0032b471c6569db2ed0af6b701894e824
Pranav Singh <139138567+pranavsinggh@users.noreply.github.com>
2023-11-05 13:03:52 +0530

    Delete backend directory

--- backend/routes/booking-routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const bookingRouter = express.Router();
-
-const { Booking, deleteBooking } = require('../controllers/booking-controller');
-
-bookingRouter.post("/", Booking);
-bookingRouter.delete('/:id', deleteBooking);
- 
-module.exports = bookingRouter; 
